23df03da76447cc07d5d7a1d581605aa708693b0
David Park & Kiran Helferty <david+kiran@appacademy.io>
2013-12-02 15:33:44 -0500

    CSS UI

--- snake_ui.js
+++ snake_ui.js
@@ -17,13 +17,53 @@
         clearInterval(this.intervalID);
         alert("You're dead!");
       } else {
-        this.$el.html(this.board.render());
+        //this.$el.html(this.board.render());
+        this.render();
       }
+    },
+
+    buildDOM: function() {
+      for(var r = 0; r < this.board.height; r++) {
+        console.log(this.board.height);
+        this.$el.append("<div class='row'></div>");
+        for(var c = 0; c < this.board.width; c++) {
+          $("div.row:last").append("<div class='cell' id='" + r + "x" + c +"'></div>");
+        };
+      };
+    },
+
+    render: function() {
+      $('div.cell').removeClass('snake');
+      $('div.cell').removeClass('apple');
+
+      var apples = this.board.apples;
+      for (var i=0; i<apples.length; i++) {
+        var coord = apples[i];
+        var id = coord.y.toString() + "x" + coord.x.toString();
+
+        $('#'+id).addClass('apple');
+      }
+
+      var segments = this.board.snake.segments;
+      for (var i=0; i<segments.length; i++) {
+        var coord = segments[i];
+        var id = coord.y.toString() + "x" + coord.x.toString();
+        $('#'+id).removeClass('apple');
+        $('#'+id).addClass('snake');
+      }
+
+
     },
 
     start: function() {
       // Construct board
       this.board = new Project.Board(15,15,5);
+
+      // Build DOM
+      this.buildDOM();
+
+      // Initial render
+      this.render();
 
       // Bind key events
       var self = this;
